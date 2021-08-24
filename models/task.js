b0b6cdc53e3f8720d02ad6d0363870cb9a8e4b76
Aditya Soni <adist98@gmail.com>
2021-08-24 07:46:05 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// Schema defines how the data will be stored in the database
 const todoSchema = new mongoose.Schema({
     task_description: {
         type: String,
