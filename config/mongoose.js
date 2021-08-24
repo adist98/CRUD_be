045f5a393deb0711e20a2304abaa40394375c11b
Aditya Soni <adist98@gmail.com>
2021-08-24 07:46:48 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+
+// database definition
 mongoose.connect('mongodb://localhost/todoapp');
+
+// Establishing the connection
 const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, "Error in connecting to the Database"));
 
 db.once('open', function(){
